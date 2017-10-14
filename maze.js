6e8665679bc20754180789d9f51076063342e309
Yogii876 <yohann.brown@gmail.com>
2017-10-13 19:45:08 -0500

    Ex3

--- maze.js
+++ maze.js
@@ -1,12 +1,26 @@
-window.onload = function(){
-var allboun = document.querySelectorAll("div#maze div.boundary");
- 	for (var x = 0; x < allboun.length; x++){
- 		allboun[x].addEventListener("mouseover", turnRed);
- 	}
- }
- function turnRed() {
+var flag = false; //keep track if the user hit any maze walls.
+ 
+ window.onload = function() {
+     var e = document.getElementById("end");
+     e.addEventListener("mouseover", gameOver);
      var allboun = document.querySelectorAll("div#maze div.boundary");
-     for (var x = 0; x < allboun.length; x++){
-         allboun[x].setAttribute("class", "boundary youlose");
+  	for (var x = 0; x < allboun.length; x++){
+		        allboun[x].addEventListener("mouseover", turnRed);
+     }
+ };
+ 
+  function turnRed() {
+ 	flag = true;
+      var allboun = document.querySelectorAll("div#maze div.boundary");
+      for (var x = 0; x < allboun.length; x++){
+          allboun[x].setAttribute("class", "boundary youlose");
+      }
+  }
+ 
+ function gameOver() {
+     if(flag) {
+         alert("Oops! You lost.");
+     } else {
+         alert("Congratulations! You win! :)");
      }
  }
