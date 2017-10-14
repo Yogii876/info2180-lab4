4c6e4ce9b6a1cd4e6ebbce96bc326c3eaa48eb43
Yogii876 <yohann.brown@gmail.com>
2017-10-13 19:52:54 -0500

    Ex4

--- maze.js
+++ maze.js
@@ -1,26 +1,36 @@
 var flag = false; //keep track if the user hit any maze walls.
- 
- window.onload = function() {
-     var e = document.getElementById("end");
-     e.addEventListener("mouseover", gameOver);
-     var allboun = document.querySelectorAll("div#maze div.boundary");
+  
+  window.onload = function() {
+      var e = document.getElementById("end");
+      e.addEventListener("mouseover", gameOver);
+     var s = document.getElementById("start");
+     s.addEventListener("click", startGame);
+      var allboun = document.querySelectorAll("div#maze div.boundary");
   	for (var x = 0; x < allboun.length; x++){
-		        allboun[x].addEventListener("mouseover", turnRed);
+		allboun[x].addEventListener("mouseover", turnRed);
      }
  };
  
-  function turnRed() {
+ function turnRed() {
  	flag = true;
-      var allboun = document.querySelectorAll("div#maze div.boundary");
-      for (var x = 0; x < allboun.length; x++){
-          allboun[x].setAttribute("class", "boundary youlose");
-      }
-  }
+     var allboun = document.querySelectorAll("div#maze div.boundary");
+     for (var x = 0; x < allboun.length; x++){
+         allboun[x].setAttribute("class", "boundary youlose");
+     }
+ }
  
  function gameOver() {
      if(flag) {
          alert("Oops! You lost.");
-     } else {
-         alert("Congratulations! You win! :)");
-     }
+      } else {
+          alert("Congratulations! You win! :)");
+      }
  }
+ 
+ function startGame() {
+ 	flag = false;
+     var allboun = document.querySelectorAll("div#maze div.boundary");
+     for (var x = 0; x < allboun.length; x++){
+         allboun[x].setAttribute("class", "boundary");
+     }
+  } 
